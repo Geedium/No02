2a7a352c98be26ae9e3f35ce6f91a92dfda589e3
Gediminas <geed0000czero@gmail.com>
2020-08-24 01:00:32 +0300

    Update database.ts

--- utils/database.ts
+++ utils/database.ts
@@ -12,7 +12,7 @@ export default async function dbConnect() : Promise<any> {
     if(connection.isConnected)
         return
 
-    const db:mongoose.Mongoose = await mongoose.connect(process.env.MONGO_URL, {
+    const db:mongoose.Mongoose = await mongoose.connect("mongodb://[testUser:test123@]85.232.131.161[:27017]", {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
